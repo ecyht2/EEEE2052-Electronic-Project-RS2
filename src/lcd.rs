4903f0102d6de0595de653e1aaf2a4ae412e8e29
ecyht2 <ecyht2@nottingham.edu.my>
2023-03-08 23:28:49 +0800

    Added documentation for LCDButtons

--- src/lcd.rs
+++ src/lcd.rs
@@ -1,13 +1,22 @@
 #[derive(Debug, PartialEq)]
+/// Buttons on the Arduino Uno LCD keypad shield.
 pub enum LCDButtons {
+    /// Right button.
     RIGHT,
+    /// Up button.
     UP,
+    /// Down button.
     DOWN,
+    /// Left button.
     LEFT,
+    /// Select button.
     SELECT,
 }
 
 impl LCDButtons {
+    /// Gets the button being pressed from the value from the ADC.
+    ///
+    /// An error would be returned if the value given is > 4096. (Invalid comparator reading).
     pub fn new(reading: u16) -> Result<Self, ()> {
         match reading {
             0..=500 => Ok(Self::RIGHT),
